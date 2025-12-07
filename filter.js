50a624da9436934e4062e6c27252ef181c825329
mgonline06-rgb <mg.online06@gmail.com>
2025-12-07 16:11:48 +0100

    Update filter.js

--- filter.js
+++ filter.js
@@ -1,18 +1,47 @@
-const badWords = [
-  "fuck","shit","bitch","cunt","bastard",
-  "nigger","nigga","retard","fag","slut","whore"
+// Simple swear filter.
+// It replaces any banned word with ***[CENSORED]***.
+// You can extend the bannedWords array with whatever you like.
+
+// NOTE: For safety I’m not hard-coding specific slurs here,
+// but you can add them as plain strings in the array.
+
+const bannedWords = [
+  "fuck",
+  "fucking",
+  "shit",
+  "bitch",
+  "asshole",
+  "cunt",
+  "dick",
+  "bastard",
+  "nigger",
+   "nig",
+"kike",
+"sewerback",
+"negro",
+"faggot",
+"sex",
+"penis",
+"pussy",
+"jiggaboo",
+"chink",
+
+
+
+  
+  // Add extra words and slurs you want to block, like:
+  // "yourBadWordHere",
 ];
 
+const pattern = new RegExp(
+  "\\b(" + bannedWords.map(w => escapeRegExp(w)).join("|") + ")\\b",
+  "gi"
+);
+
 export function filterBadWords(text) {
+  return text.replace(pattern, "***[CENSORED]***");
+}
 
-  let lowered = text.toLowerCase();
-
-  for (let bad of badWords) {
-    if (lowered.includes(bad)) {
-      const stars = "*".repeat(bad.length);
-      const regex = new RegExp(bad, "gi");
-      text = text.replace(regex, stars);
-    }
-  }
-  return text;
+function escapeRegExp(str) {
+  return str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
 }
