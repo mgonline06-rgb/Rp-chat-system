b61fe5b677b52bda4cae68da84a08e3b6bc79b1c
mgonline06-rgb <mg.online06@gmail.com>
2025-12-07 16:30:55 +0100

    Update swearFilter.js

--- swearFilter.js
+++ swearFilter.js
@@ -1,11 +1,10 @@
-// Fantasy-safe explicit and profanity filter
 export function filterBadWords(text) {
 
   const bannedWords = [
-    "fuck","shit","bitch","asshole","bastard","cunt",
-    "pussy","penis","vagina","dick","sex","slut","whore",
-    "nigger","nigga","rape","rapist","faggot","cum","anal",
-    "cock","blowjob","handjob","semen","orgy","fetish"
+    "fuck", "shit", "bitch", "asshole", "bastard", "cunt",
+    "pussy", "penis", "vagina", "dick", "sex", "slut", "whore",
+    "nigger", "nigga", "rape", "rapist", "faggot", "cum", "anal",
+    "cock", "blowjob", "handjob", "semen", "orgy", "fetish"
   ];
 
   let cleaned = text;
@@ -16,5 +15,4 @@ export function filterBadWords(text) {
   }
 
   return cleaned;
-}
 }
